f693e339d0915c5f1764617c5691b5bdda5b16c2
prubac <pawel.rubach@sgh.waw.pl>
2022-11-10 11:04:25 +0100

    Blocks with functions

---
+++ blocks_func.py
@@ -0,0 +1,13 @@
+
+def sum(a, b):
+    i = 7
+    print(f'c={c}')
+    print(f'd={d}')
+    print(f'i={i} - inside function')
+    return a + b
+
+c = 5
+d = 8
+
+print('sum(c,d)={}'.format(sum(c, d)))
+print(f'i={i} outside function')
