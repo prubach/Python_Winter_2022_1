2bbfc60ad2195262daad8e6cfeb36bfba8e9e260
prubac <pawel.rubach@sgh.waw.pl>
2022-11-10 10:58:18 +0100

    Blocks - var visibility

---
+++ blocks.py
@@ -0,0 +1,18 @@
+a = 5
+
+for i in range(3):
+    print(f'i={i}')
+    print(f'a={a}')
+    b = 7
+
+print('------')
+print(f'i={i}')
+print(f'b={b}')
+
+if a > 4:
+    if b < 10:
+        c = 10
+    else:
+        c = 5
+
+print(f'c={c}')
