c88136a8c45d5160734ee83c83cdccbe7cf918ba
prubac <pawel.rubach@sgh.waw.pl>
2022-11-10 10:23:07 +0100

    Email checking - @ position checked

--- email_check.py
+++ email_check.py
@@ -3,15 +3,22 @@ email2 = 'pawel.rubach#sgh.waw.pl'
 email3 = 'pawel.rubach@.pl'
 email4 = '@sgh.pl'
 
+#['pawel.rubach@sgh.waw.pl', 'pawel.rubach@.pl']
 
 def check_email(email):
-    if email:
-        return True
-    else:
+    #TODO implement real checks
+    if not email:
         return False
+    if email.find('@')<=0 or email.find('@')!=email.rfind('@'):
+        print('@ not found or too many of them')
+        return False
+    return True
+
+
 
 
 for em in [email1, email2, email3, email4]:
+    print('--------------------')
     print('{} : {}'.format(em, check_email(em)))
     #print(check_email(em))
 
