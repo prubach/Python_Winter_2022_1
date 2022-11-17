fb1a1f651f43e25f3661305a24473f1d568d7543
prubac <pawel.rubach@sgh.waw.pl>
2022-11-17 10:48:04 +0100

    Lists - exercises solved

--- lists.py
+++ lists.py
@@ -23,4 +23,27 @@ print(l1)
 
 l2 = [2525, 12536, 585895, 'Hello Python 3', 27848]
 #TODO Please extract the word "Python" from l2
+my_elem = l2[3]
+print(my_elem)
+print(my_elem[6:12])
+
 #TODO Sum up all numbers in l2
+# print('--------------')
+# l2.remove('Hello Python 3')
+# print(l2)
+# #my_elem1=l2
+# sum = sum(l2)
+# print(sum)
+
+print('--------------')
+total_sum = 0
+for item in l2:
+    if type(item) is int:
+        total_sum = total_sum + item
+print(total_sum)
+print(l2)
+
+#l3 = [ x for x in l2 if type(x)==int]
+l3 = [ x for x in l2 if isinstance(x, (int, float, complex))]
+print(l3)
+print(sum(l3))
