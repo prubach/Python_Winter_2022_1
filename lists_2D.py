e8696812e445bcaf7a5123a7fb64d2cda0b0971c
prubac <pawel.rubach@sgh.waw.pl>
2022-11-17 11:12:57 +0100

    Sets and 2D lists

---
+++ lists_2D.py
@@ -0,0 +1,11 @@
+l_2d = [[24, 3, 36],
+        [7, 3, 6],
+        [7, 5, 8]]
+print(l_2d[1][2])
+in_list = l_2d[1]
+print(in_list)
+print(in_list[2])
+#TODO - sum elements of the whole list
+#TODO - sum all values in columns
+#TODO - sum all values in rows
+
---
+++ lists_set.py
@@ -0,0 +1,7 @@
+l1 = [2525, 12536, 585895, 27848, 2525, 12536]
+print(l1)
+s1 = set(l1)
+print(s1)
+#print(s1[0])
+for el in s1:
+    print(el)
