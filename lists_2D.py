196f8e5fbf751f9ca4d7553281cbe3a93cbd06d4
prubac <pawel.rubach@sgh.waw.pl>
2022-11-24 10:20:21 +0100

    Sum elements in 2D list

--- lists_2D.py
+++ lists_2D.py
@@ -5,7 +5,19 @@ print(l_2d[1][2])
 in_list = l_2d[1]
 print(in_list)
 print(in_list[2])
-#TODO - sum elements of the whole list
+
 #TODO - sum all values in columns
+
 #TODO - sum all values in rows
+
+#TODO - sum elements of the whole list
+sum_all = 0
+for i in l_2d:
+    for j in i:
+        sum_all += j
+
+print(f'Sum all: {sum_all}')
+
+
+
 
