0eafe2bd495b71ae63c8f04e2e28b4d2a537e61b
prubac <pawel.rubach@sgh.waw.pl>
2022-12-01 10:34:16 +0100

    Reading a file from various locations on the drive

--- file_reading.py
+++ file_reading.py
@@ -1,4 +1,26 @@
-my_file = 'hello.txt'
+import io
+import os
+
+my_folder = r'c:\users\prubac\Desktop'
+
+my_file_location = r'..\hello.txt'
+
+
+my_file = os.path.join(my_folder, my_file_location)
+print(os.path.dirname(my_file))
+#print(os.path.exists(my_file))
+if os.path.exists(my_file):
+    print('{}: exists'.format(my_file))
+else:
+    print('{}: does not exist'.format(my_file))
+print(my_file)
+#my_file = r'H:\Python_Winter_2022_1\hello.txt'  # raw string - no special symbols
+#my_file = 'H:\\Python_Winter_2022_1\\hello.txt'  # raw string - no special symbols
+#my_file = 'H:/Python_Winter_2022_1/hello.txt'  # raw string - no special symbols
+
+print('Current path: {}'.format(os.getcwd()))
+print('Reading file from: {}'.format(os.path.abspath(my_file)))
+
 
 # f = open(my_file, 'r')
 # lines = f.readlines()
@@ -7,6 +29,7 @@ my_file = 'hello.txt'
 #     i += 1
 #     print(f'{i}: {line}', end='')
 # f.close()
+
 
 with open(my_file, 'r') as f:
     lines = f.readlines()
---
+++ list_directory.py
@@ -0,0 +1,4 @@
+import os
+
+my_folder = r'c:\users\prubac\Desktop'
+print(os.listdir(my_folder))
