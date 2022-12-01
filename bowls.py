8d553b8bdeb90797ba8fda000021ed154659de29
prubac <pawel.rubach@sgh.waw.pl>
2022-12-01 10:39:26 +0100

    Bowls

---
+++ bowls.py
@@ -0,0 +1,10 @@
+#        * * * * * *
+#         * * * * *
+#          * * * *
+#           * * *
+#            * *
+#             *
+
+n = 6
+
+# How many bowls do I have given the n - number of rows
